9d81882c5bf930a3d7468e5b2740f3a2e4af0a79
Mark Strickland <stricklandmes@gmail.com>
2019-11-12 12:36:40 -0700

    rev actor and identifier

--- src/actor.py
+++ src/actor.py
@@ -4,15 +4,26 @@ class Actor:
     # input: x (state), V_hat (value), delta_hjb (Bellman Error)
     # output: u_hat (input)
     def __init__(self, params):
+        # init params
         self.delta_t = params[0]
         self.R11 = params[1]
         self.R22 = params[2]
-        self.Gamma_11a = params[3]
-        self.Gamma_12a = params[4]
-        self.Gamma_13a = params[5]
-        self.Gamma_21a = params[6]
-        self.Gamma_22a = params[7]
-        self.Gamma_23a = params[8]
+        self.R12 = params[3]
+        self.R21 = params[4]
+        self.Q1 = params[5]
+        self.Q2 = params[6]
+        self.Gamma_11a = params[7]
+        self.Gamma_12a = params[8]
+        self.Gamma_13a = params[9]
+        self.Gamma_21a = params[10]
+        self.Gamma_22a = params[11]
+        self.Gamma_23a = params[12]
+        x0 = params[13]
+        self.update_phi1_phi2(x0)
+        # init output
+        self.u1_hat = 0.0
+        self.u2_hat = 0.0
+
     #
     def g1(self, x):
         x1, x2 = x
@@ -26,15 +37,64 @@ class Actor:
         row2 = np.sin(4.0 * x1**2) + 2.0
         return np.array([[row1], [row2]])
     #
-    def updateWeights(self):
-        ''' Equations 51 '''
+    def update_phi1_phi2(self, x):
+        x1, x2 = x
+        self.phi1 = [x1**2, x1*x2, x2**2]
+        self.phi2 = [x1**2, x1*x2, x2**2]
+    #
+    def update_phi1_phi2_prime(self, x):
+        phi1_old = self.phi1.copy()
+        phi2_old = self.phi2.copy()
+        self.update_phi1_phi2(x)
+        self.phi1_prime = (self.phi1 - phi1_old) / self.delta_t
+        self.phi2_prime = (self.phi2 - phi2_old) / self.delta_t
+    #
+    def update_omega(self, x):
+        self.update_phi1_phi2_prime(x)
+        #  need help here
+        self.F_hat_u_hat = 0.0
+        self.F_hat_u_hat = 0.0
+        #
+        self.omega_1 = self.phi1_prime * self.F_hat_u_hat
+        self.omega_2 = self.phi2_prime * self.F_hat_u_hat
+    #
+    def update_local_cost(self, x):
+        u1 = self.u1_hat
+        u2 = self.u2_hat
+        self.r1 = np.transpose(x) * self.Q1 * x + np.transpose(u1) * self.R11 * u1 + \
+                    np.transpose(u2) * self.R12 * u2
+        self.r2 = np.transpose(x) * self.Q2 * x + np.transpose(u2) * self.R22 * u2 + \
+                    np.transpose(u1) * self.R21 * u1
+    #
+    def updateWeights(self, x):
+        ''' Equations 50 and 51 '''
+        # update G1, G2, G12, G21
+        G1 = self.g1(x) * (1.0/self.R11) * self.g1(x)
+        G2 = self.g2(x) * (1.0/self.R22) * self.g2(x)
+        G12 = self.g2(x) * (1.0/self.R22) * self.R12 * (1.0/self.R22) * self.g2(x)
+        G21 = self.g1(x) * (1.0/self.R11) * self.R21 * (1.0/self.R11) * self.g1(x)
+        # update partial E_a / partial W1a_hat
+        delta_hjb1 = np.transpose(self.W1c_hat) * self.omega_1 + self.r1
+        delta_hjb2 = np.transpose(self.W2c_hat) * self.omega_2 + self.r1
+        deriv_Ea_W1a_hat = np.transpose(self.W1a_hat - self.W1c_hat) * self.phi1_prime * \
+                            G1 * np.transpose(self.phi1_prime) * delta_hjb1 + \
+                            (np.transpose(self.W1a_hat) * self.phi1_prime * G21 - \
+                            np.transpose(self.W2c_hat) * self.phi2_prime * G1) * \
+                            np.transpose(self.phi1_prime) * delta_hjb2
+
+        deriv_Ea_W2a_hat = (np.transpose(self.W2a_hat) * self.phi2_prime * G12 - \
+                            np.transpose(self.W1c_hat) * self.phi1_prime * G2) * np.transpose(self.phi2_prime) * delta_hjb1 + \
+                            np.transpose(self.W2a_hat - self.W2c_hat) * self.phi2_prime * G2 * \
+                            np.transpose(self.phi2_prime) * delta_hjb2
+        #
+        # update W1a_hat and W2a_hat
         #  see FN3 regarding substituting for projection algorithm
-        W1a_hat_dot = -1.0 * self.Gamma_11a * deriv_Ea_W1a / (np.sqrt(1.0 + self.omega_1 * self.omega_1)) \
+        W1a_hat_dot = -1.0 * self.Gamma_11a * deriv_Ea_W1a_hat / (np.sqrt(1.0 + self.omega_1 * self.omega_1)) \
                         - self.Gamma_12a * (self.W1a_hat - self.W1c_hat) \
                         - self.Gamma_13a * self.W1a_hat
         self.W1a_hat = W1a_hat_dot * self.delta_t + self.W1a_hat
         #
-        W2a_hat_dot = -1.0 * self.Gamma_21a * deriv_Ea_W2a / (np.sqrt(1.0 + self.omega_2 * self.omega_2)) \
+        W2a_hat_dot = -1.0 * self.Gamma_21a * deriv_Ea_W2a_hat / (np.sqrt(1.0 + self.omega_2 * self.omega_2)) \
                         - self.Gamma_22a * (self.W2a_hat - self.W2c_hat) \
                         - self.Gamma_23a * self.W2a_hat
         self.W2a_hat = W2a_hat_dot * self.delta_t + self.W2a_hat
@@ -43,9 +103,14 @@ class Actor:
         ''' Equations 44 '''
         # get input
         x = input[0]
+        # update omega - omega is the critic NN regressor vector
+        self.update_omega(x)
+        # update local costs r1 and r2
+        self.update_local_cost(x)
         # update weights
-        self.updateWeights()
+        self.updateWeights(x)
         # update u1, u2
-        u1_hat = (-1.0/2.0)*(1.0/self.R11) * self.g1(x) * self.phi1_prime(x) * self.W1a_hat
-        u2_hat = (-1.0/2.0)*(1.0/self.R22) * self.g2(x) * self.phi2_prime(x) * self.W2a_hat
-        return np.array([u1_hat], [u2_hat])
+        self.u1_hat = (-1.0/2.0)*(1.0/self.R11) * np.transpose(self.g1(x)) * np.transpose(self.phi1_prime(x)) * self.W1a_hat
+        self.u2_hat = (-1.0/2.0)*(1.0/self.R22) * np.transpose(self.g2(x)) * np.transpose(self.phi2_prime(x)) * self.W2a_hat
+        output = np.array([self.u1_hat], [self.u2_hat])
+        return output
--- src/identifier.py
+++ src/identifier.py
@@ -1,15 +1,28 @@
 import numpy as np
+import math
 
 class Identifier:
     #  Identifier:  input is x_tilde; output is x_hat_dot
     def __init__(self, params):
-        self.x_tilde_0 = params[0]
-        self.k_gain = params[1]
-        self.alpha = params[2]
-        self.gamma = params[3]
-        self.beta1 = params[4]
-        self.delta_t = params[5]
+        # init params
+        self.delta_t = params[0]
+        self.x_tilde_0 = params[1]
+        self.k_gain = params[2]
+        self.alpha = params[3]
+        self.gamma_f = params[4]
+        self.beta_1 = params[5]
+        self.Gamma_wf = params[6]
+        self.Gamma_vf = params[7]
+        self.Wf_hat = params[8]         # need help here
+        self.Vf_hat = params[9]         # need help here
         self.nu = 0.0
+        # init values of computed values
+        self.sigf_hat = 0.0             # need help here
+        self.sigf_hat_old = 0.0         # need help here 
+        self.x_hat_dot = 0.0
+    #
+    def sigmoid(self, x):
+        return 1.0 / (1.0 + math.exp(-x))
     #
     def g1(self, x):
         x1, x2 = x
@@ -30,12 +43,24 @@ class Identifier:
         return np.array([[row1], [row2]])
     #
     def update_nu(self, x_tilde):
-        nu_dot = (self.k_gain * self.alpha + self.gamma) * x_tilde + self.beta1 * np.sign(x_tilde)
+        nu_dot = (self.k_gain * self.alpha + self.gamma_f) * x_tilde + self.beta_1 * np.sign(x_tilde)
         self.nu = nu_dot * self.delta_t + self.nu
     #
-    def updateWeights(self):
-        self.Wf_hat = self.Wf_hat
-        self.sigf_hat = self.sigf_hat
+    def updateWeights(self, x_tilde, x_hat):
+        # need help here
+        self.sigf_hat_prime = (self.sigf_hat - self.sigf_hat_old) / self.delta_t
+        # update Vf_hat
+        Vf_hat_dot = self.proj( self.Gamma_vf * self.x_hat_dot * np.transpose(x_tilde) * \
+                                np.transpose(self.Wf_hat) * self.sigf_hat_prime )
+        self.Vf_hat = Vf_hat_dot * self.delta_t + self.Vf_hat
+        # update Wf_hat
+        Wf_hat_dot = self.proj( self.Gamma_wf * self.sigf_hat_prime * np.transpose(self.Vf_hat) * \
+                                self.x_hat_dot * np.transpose(x_tilde) )
+        self.Wf_hat = Wf_hat_dot * self.delta_t + self.Wf_hat
+        #
+        # update sigf_hat
+        self.sigf_hat_old = self.sigf_hat.copy()
+        self.sigf_hat = self.sigmoid(np.transpose(self.Vf_hat) * x_hat)
 
     def nextStateHat(self, input):
         '''
@@ -47,15 +72,15 @@ class Identifier:
         u1 = input[2]
         u2 = input[3]
         # update weights and nu
-        self.updateWeights()
-        self.update_nu()
+        self.updateWeights(x_tilde, x_hat)
+        self.update_nu(x_tilde)
         # calculate mu
         mu = self.k_gain * (x_tilde - self.x_tilde_0) + self.nu
         if self.num_players == 2:
-            x_hat_dot = self.Wf_hat * self.sigf_hat + self.g1(x) * u1 + self.g2(x) * u2 + mu
+            self.x_hat_dot = self.Wf_hat * self.sigf_hat + self.g1(x) * u1 + self.g2(x) * u2 + mu
         else:
             u3 = input[4]
-            x_hat_dot = self.Wf_hat * self.sigf_hat + self.g1(x) * u1 + self.g2(x) * u2 + self.g3(x) * u3 + mu
+            self.x_hat_dot = self.Wf_hat * self.sigf_hat + self.g1(x) * u1 + self.g2(x) * u2 + self.g3(x) * u3 + mu
         #
         # return
-        return x_hat_dot
+        return self.x_hat_dot
