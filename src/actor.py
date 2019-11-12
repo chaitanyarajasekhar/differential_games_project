94ff9e9fb1f952c3e5bf360e3f143b05a553b4f3
Mark Strickland <stricklandmes@gmail.com>
2019-11-11 23:30:29 -0700

    rev actor

--- src/actor.py
+++ src/actor.py
@@ -7,6 +7,12 @@ class Actor:
         self.delta_t = params[0]
         self.R11 = params[1]
         self.R22 = params[2]
+        self.Gamma_11a = params[3]
+        self.Gamma_12a = params[4]
+        self.Gamma_13a = params[5]
+        self.Gamma_21a = params[6]
+        self.Gamma_22a = params[7]
+        self.Gamma_23a = params[8]
     #
     def g1(self, x):
         x1, x2 = x
@@ -22,6 +28,7 @@ class Actor:
     #
     def updateWeights(self):
         ''' Equations 51 '''
+        #  see FN3 regarding substituting for projection algorithm
         W1a_hat_dot = -1.0 * self.Gamma_11a * deriv_Ea_W1a / (np.sqrt(1.0 + self.omega_1 * self.omega_1)) \
                         - self.Gamma_12a * (self.W1a_hat - self.W1c_hat) \
                         - self.Gamma_13a * self.W1a_hat
