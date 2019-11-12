94ca0eaf58a310411817a2798dc5f236706aa464
Chaitanya Rajasekhar <crajase1@asu.edu>
2019-11-12 13:38:49 -0700

    added 2player game plant functions

--- src/plant.py
+++ src/plant.py
@@ -1,11 +1,56 @@
 import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
 
-class Plant:
+def stateEquation2P(state,t,input_1,input_2):
+    '''
+        equations 68 and 69
+    '''
+    # current state
+    x1   = state[0]
+    x2   = state[1]
+    # state equations
+    f_x   = np.array([x2 - 2*x1, \
+            -0.5*x2+0.25*x2*(np.cos(2*x1)+2)**2 + 0.25*x2*(np.sin(4*x1**2)+2)**2])
+    g1_x  = np.array([0,np.cos(2*x1)+2])
+    g2_x  = np.array([0,np.sin(4*x1**2)+2])
+    x_dot = f_x + g1_x * input_1 + g2_x * input_2
 
-    def __init__(self,params):
-        pass
+    return x_dot
 
+class Plant2Player:
+    def __init__(self,params=None):
+        '''
+            intializing the equations
+        '''
+        self.state        = np.array([1,2])
+        self.dt           = 0.0025 # change it take the value from params
+        self.time         = []
+        self.state_traj   = []
+        self.current_time = 0.0
+        self.state_traj.append(self.state.copy())
+        self.time.append(0.)
 
-    def nextState(self,input):
-        ''' '''
-        pass
+    def nextState(self,input=None):
+        '''
+        '''
+        next_state = odeint(stateEquation2P,self.state,np.array([0.0, self.dt]),args=(0,0))
+
+        self.state        = next_state[-1,:]
+        self.current_time = self.current_time + self.dt
+
+        self.state_traj.append(next_state[-1,:])
+        self.time.append(self.current_time)
+
+def main():
+
+    sim = Plant2Player()
+
+    for i in range(10):
+        sim.nextState()
+
+    print(sim.state_traj)
+    print(sim.time)
+
+if __name__ == "__main__":
+    main()
